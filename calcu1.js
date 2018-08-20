6fbd85e95d188850fbcf018e1acb9ea994a22c88
nicolasculerier <nicolas.culerier@laposte.net>
2018-08-20 16:25:30 +0200

    Mise en chauffe done

--- calcu1.js
+++ calcu1.js
@@ -1,31 +1,29 @@
-function my_max(){
-
-var array = []
-
-var readline = require('readline');
-
-var rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-rl.question("Entrez une liste de nombres : ", function(list) {
-  array.push(list)
-  var maximum = Math.max.apply(list);
-  console.log("Le maximum de votre liste est :", maximum);
-
-  rl.close();
-});
-
+function my_max(array){
+console.log("Le plus grand nombre est " + Math.max(...array));
 }
 
+function vowel_count(string){
 
-function vowel_count(){
-let vowels = ["a", "e", "i", "o", "u", "y"];
-
+var arr = string.split('');
+  var i = 0;
+arr.forEach(function(element){
+  var vowels = ["a", "e", "i", "o", "u", "y"];
+  if (vowels.includes(element)){
+    i = i + 1;
+  }
+})
+console.log("Le nombre de voyelles est "+ i)
 }
 
-function reverse(){
+function reverse(string){
+  var arr = string.split('');
+  var reversed = arr.reverse();
+  var revstring = reversed.join('')
+  console.log(revstring)
 }
 
-my_max();
+my_max([1,2,1000,278,45,2,1456,235,245,54,79,12316,50,47,9,632]);
+
+vowel_count("hello")
+
+reverse("Bonjour Monde !")
